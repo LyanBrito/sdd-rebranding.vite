56411f519cfb8e4ea94235023cd660cc58b1f743
lyanbrito <lyanbrito613@gmail.com>
2026-04-30 22:26:36 -0300

    feat: added animated language dropdown & navbar

--- src/components/LangHandler.tsx
+++ src/components/LangHandler.tsx
@@ -1,13 +1,51 @@
+"use client"
+
 import * as I from "../assets/icons/Icons.tsx";
 import * as S from "./styles.ts";
-
+import {useState} from "react";
+import {AnimatePresence, motion} from "motion/react"
 
 export default function LangHandler() {
+    const [isVisible, setIsVisible] = useState(false)
+
     return (
-        <S.DropdownContainer>
-            <I.GlobeIcon />
-            <S.DDLabel>Change Region</S.DDLabel>
-            <I.ArrowIcon />
-        </S.DropdownContainer>
+        <>
+            <S.DropdownContainer
+                as={motion.div}
+                onClick={() => setIsVisible(!isVisible)}
+                whileTap={{scale: 0.98}}
+            >
+                {/*Como fiz com o nav, posso tacar as animações direto na criação do styled-component*/}
+                <I.GlobeIcon/>
+                <S.DDLabel
+                    as={motion.p}
+                    whileHover={{scale: 0.98}}
+                >Change Region</S.DDLabel>
+                <I.ArrowIcon/>
+            </S.DropdownContainer>
+            <AnimatePresence initial={false}>
+                {isVisible ?
+                    <S.LangOptionsWrapper
+                        as={motion.div}
+                        initial={{opacity: 0, y: -30}}
+                        animate={{opacity: 1, y: 0}}
+                        exit={{opacity: 0, y: -30}}
+                    >
+                        <S.LangOptions>
+                            <span>🇧🇷</span>
+                            Brasil
+                        </S.LangOptions>
+                        <S.LangOptions>
+                            <span>🇺🇸</span>
+                            U.S.A
+                        </S.LangOptions>
+                        <S.LangOptions>
+                            <span>🇪🇸</span>
+                            España
+                        </S.LangOptions>
+                    </S.LangOptionsWrapper> : null
+                }
+            </AnimatePresence>
+        </>
     )
 }
--- src/components/styles.ts
+++ src/components/styles.ts
@@ -1,19 +1,26 @@
 import styled from "styled-components";
 import {Colors, Fonts} from "../styles/util/variables.ts";
+import {motion} from "motion/react";
 
 // Navbar
 interface NavProps {
     actualPage?: boolean;
 }
 
-export const NavConatiner = styled.nav`
+export const NavContainer = styled.nav`
     display: flex;
     height: 64px;
     align-items: center;
     width: 17.96875%;
     gap: 3px;
 `
-export const NavLink = styled.a<NavProps>`
+
+export const NavLink = styled(motion.a).attrs<NavProps>(() => ({
+//     animation props
+    whileHover: {
+        fontWeight: 600,
+    }
+}))<NavProps>`
     display: flex;
     padding: 8px 12px;
     justify-content: center;
@@ -28,9 +35,6 @@ export const NavLink = styled.a<NavProps>`
     width: 100%;
     outline: ${({actualPage}) => (actualPage ? '2px solid #000' : '')};
     outline-offset: ${({actualPage}) => (actualPage ? '-1px' : '')};
-    &:hover {
-        font-weight:  ${({actualPage}) => (actualPage ? 700 : 600)};
-    }
 `
 
 // Header
@@ -56,4 +60,34 @@ export const DDLabel = styled.p`
     font-style: normal;
     font-weight: 600;
     line-height: normal;
+`
+export const LangOptionsWrapper = styled.div`
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 8px;
+    width: 147px;
+    right: 60px;
+    top: 10%;
+`
+
+export const LangOptions = styled(motion.p).attrs({
+    whileHover: {scale: 1.08}
+})`
+    color: ${Colors.black};
+    font-family: ${Fonts.primalFF};
+    font-size: ${Fonts.sizeTN};
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 4px;
+
+    span {
+        font-size: ${Fonts.sizeLG};
+    }
 `
