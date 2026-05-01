435a81c9bd290bb85049466a2c6b526a9279b51d
lyanbrito <lyanbrito613@gmail.com>
2026-04-30 22:25:34 -0300

    fix: removed unused animation

--- src/styles/base/globalStyles.ts
+++ src/styles/base/globalStyles.ts
@@ -8,8 +8,6 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
         margin: 0;
         padding: 0;
-        transition: all 0.15s linear;
-    //    transição temporária para desenvolvimento
     }
 
     html {
