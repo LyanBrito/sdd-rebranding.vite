0bc3765e6caf215a53e8801d50f6dd18f5ec185d
lyanbrito <lyanbrito613@gmail.com>
2026-05-04 16:43:34 -0300

    test: testing component styles

---
+++ src/components/ProjectsSection.tsx
@@ -0,0 +1,16 @@
+import Card from "./Card.tsx";
+import PH from "../assets/hero.png";
+
+
+export default function ProjectsSection() {
+    return (
+        <>
+            <Card cardNum={"00"} cardTitle={"Suporte de Domingo"} cardImg={PH} secDescription={"des"} linkRef={"/"} linkLabel={"link"} />
+            <Card isProject cardNum={"00"} cardTitle={"Suporte de Domingo"} cardImg={PH} secDescription={"des"} linkRef={"/"} linkLabel={"link"} />
+            <Card isProject cardNum={"00"} cardTitle={"Suporte de Domingo"} cardImg={PH} secDescription={"sdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjkl"} linkRef={"/"} linkLabel={"link"} />
+            <Card isProject cardNum={"00"} cardTitle={"Suporte de Domingo"} cardImg={PH} secDescription={"sdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjkl"} linkRef={"/"} linkLabel={"link"} />
+            <Card isProject cardNum={"00"} cardTitle={"Suporte de Domingo"} cardImg={PH} secDescription={"sdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjklsdfghjklkjhgfdsdfghjkl"} linkRef={"/"} linkLabel={"link"} />
+
+        </>
+    )
+}
