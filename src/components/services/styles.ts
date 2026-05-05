40ef6a746720dfbd5b3d31ae14f8db260e7f56e6
lyanbrito <lyanbrito613@gmail.com>
2026-05-05 00:35:57 -0300

    styled: services

---
+++ src/components/services/styles.ts
@@ -0,0 +1,100 @@
+import styled from "styled-components";
+import {Colors, Fonts} from "../../styles/util/variables.ts";
+import {motion} from "motion/react";
+
+// Card
+export const CardContainer = styled(motion.div).attrs({
+    whileHover: {
+        scale: 1.04,
+        boxShadow: "3px 3px 0 0 #FFC700, 3px 3px 0 2px #000, 7px 7px 0 0 #74247A, 7px 7px 0 2px #000",
+    }
+})`
+    display: flex;
+    width: 100%;
+    padding: 25px 30px;
+    flex-direction: column;
+    gap: 47px;
+    justify-content: space-between;
+    align-items: flex-end;
+    border-radius: 24px;
+    border: 3px solid ${Colors.black};
+    background-color: ${Colors.white} ;
+    box-shadow: 4px 4px 0 0 ${Colors.purple}, 0 0 0 0 'transparent', 0 0 0 0 'transparent', 0 0 0 0 'transparent';
+`
+export const CardBody = styled.div`
+    display: flex;
+    align-items: flex-start;
+    gap: 16px;
+    width: 100%;
+`
+
+export const CardTitle = styled.h5`
+    color: ${Colors.black};
+    font-family: ${Fonts.primalFF};
+    font-size: ${Fonts.sizeLG};
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    width: 100%;
+`
+export const CardLink = styled(motion.a).attrs({
+    whileHover: {
+        scale: 1.04,
+        fontWeight: 710
+    }
+})`
+    color: ${Colors.black};
+    font-family: ${Fonts.primalFF};
+    font-size: ${Fonts.sizeTN};
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    
+`
+export const CardText = styled.p`
+    color: ${Colors.black};
+    font-family: ${Fonts.primalFF};
+    font-size: ${Fonts.sizeTN};
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    width: 100%;
+`
+// Section
+export const SectionContainer = styled(motion.div)`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 60px;
+    gap: 40px;
+    background-color: ${Colors.purple};
+`
+
+export const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    gap: 20px;
+    padding: 40px;
+    width: 100%;
+`;
+
+export const SectionTitle = styled.h4`
+    color: ${Colors.white};
+    font-family: ${Fonts.primalFF};
+    font-size: ${Fonts.sizeXL};
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+`
+
+export const SectionText = styled.p`
+    font-family: ${Fonts.primalFF};
+    color: ${Colors.white};
+    font-size: ${Fonts.sizeLG};
+    margin-top: -20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    width: 100%;
+
+`
